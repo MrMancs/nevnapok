8031bf35aa55fddd554fb1b3e8720a2956d6fbc6
MrMancs <marcell.szatmari2007@gmail.com>
2025-12-15 20:56:12 +0100

    Modify files

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Form } from "react-bootstrap";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function RadioButtons(props) {
+  const handleRadioButtons = (e) => {
+    const selectedOption = e.target.id;
+    props?.radioOption(selectedOption);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Form style={{ marginTop: "20px" }}>
+      <Form.Check
+        inline
+        name="nameDay"
+        type="radio"
+        id="nameSearch"
+        label="Keresés név alapján"
+        onChange={(e) => handleRadioButtons(e)}
+      />
 
-export default App
+      <Form.Check
+        inline
+        name="nameDay"
+        type="radio"
+        id="dateSearch"
+        label="Keresés dátum alapján"
+        onChange={(e) => handleRadioButtons(e)}
+      />
+    </Form>
+  );
+}
